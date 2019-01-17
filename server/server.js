34a3586fb298ee9be35b49367ea0d4117489da5c
cadonuno <ricardoapjunior@hotmail.com>
2019-01-17 20:56:19 +0100

    Test POST /todos

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+  var todo = new Todo({
+    text: request.body.text
+  })
+  todo.save().then((doc) => {
+    response.send(doc);
+  }, (e) => {
+    response.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+
+module.exports = {app}
