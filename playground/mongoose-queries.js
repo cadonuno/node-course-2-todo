702c2aabf4321a7c939e3fcf23486d044f3145f2
cadonuno <ricardoapjunior@hotmail.com>
2019-01-17 21:52:17 +0100

    GET Todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,5 +1,3 @@
-const {ObjectID} = require("mongodb");
-
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require("mongodb");
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -26,6 +27,25 @@ app.get('/todos', (request, response) => {
   }, (e) => {
     response.status(400).send(e);
   })
+});
+
+//if invalid id return 404
+//findById(id) (success or error - return empty 400)
+//if Todo send it back, if no todo send back a 404
+app.get('/todos/:id', (request, response) => {
+  var id = request.params.id;
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return response.status(404).send();
+    }
+    response.send({todo});
+  }).catch((e) => {
+    response.status(404).send();
+  });
 });
 
 app.listen(3000, () => {
